56f3feedf9ec31b294aa0a20a2a12f7e05c275ed
ShinTetsu <qin.zhe@za.ivis.co.jp>
2024-09-04 11:51:05 +0900

    test commit

--- scripts/populate-instance.sh
+++ scripts/populate-instance.sh
@@ -131,46 +131,46 @@ ${INVENIO_WEB_INSTANCE} stats partition create $(date -d 'year' +%Y)
 
 # sphinxdoc-index-initialisation-begin
 # ${INVENIO_WEB_INSTANCE} index destroy --yes-i-know
-${INVENIO_WEB_INSTANCE} index init
-sleep 20
-${INVENIO_WEB_INSTANCE} index queue init
-# sphinxdoc-index-initialisation-end
+# ${INVENIO_WEB_INSTANCE} index init
+# sleep 20
+# ${INVENIO_WEB_INSTANCE} index queue init
+# # sphinxdoc-index-initialisation-end
 
-# elasticsearch-ilm-setting-begin
-curl -XPUT 'http://'${INVENIO_ELASTICSEARCH_HOST}':9200/_ilm/policy/weko_stats_policy' -H 'Content-Type: application/json' -d '
-{
-  "policy":{
-    "phases":{
-      "hot":{
-        "actions":{
-          "rollover":{
-            "max_size":"50gb"
-          }
-        }
-      }
-    }
-  }
-}'
-event_list=('celery-task' 'item-create' 'top-view' 'record-view' 'file-download' 'file-preview' 'search')
-for event_name in ${event_list[@]}
-do
-  curl -XPUT 'http://'${INVENIO_ELASTICSEARCH_HOST}':9200/'${SEARCH_INDEX_PREFIX}'-events-stats-'${event_name}'-000001' -H 'Content-Type: application/json' -d '
-  {
-    "aliases": {
-      "'${SEARCH_INDEX_PREFIX}'-events-stats-'${event_name}'": {
-        "is_write_index": true
-      }
-    }
-  }'
-  curl -XPUT 'http://'${INVENIO_ELASTICSEARCH_HOST}':9200/'${SEARCH_INDEX_PREFIX}'-stats-'${event_name}'-000001' -H 'Content-Type: application/json' -d '
-  {
-    "aliases": {
-      "'${SEARCH_INDEX_PREFIX}'-stats-'${event_name}'": {
-        "is_write_index": true
-      }
-    }
-  }'
-done
+# # elasticsearch-ilm-setting-begin
+# curl -XPUT 'http://'${INVENIO_ELASTICSEARCH_HOST}':9200/_ilm/policy/weko_stats_policy' -H 'Content-Type: application/json' -d '
+# {
+#   "policy":{
+#     "phases":{
+#       "hot":{
+#         "actions":{
+#           "rollover":{
+#             "max_size":"50gb"
+#           }
+#         }
+#       }
+#     }
+#   }
+# }'
+# event_list=('celery-task' 'item-create' 'top-view' 'record-view' 'file-download' 'file-preview' 'search')
+# for event_name in ${event_list[@]}
+# do
+#   curl -XPUT 'http://'${INVENIO_ELASTICSEARCH_HOST}':9200/'${SEARCH_INDEX_PREFIX}'-events-stats-'${event_name}'-000001' -H 'Content-Type: application/json' -d '
+#   {
+#     "aliases": {
+#       "'${SEARCH_INDEX_PREFIX}'-events-stats-'${event_name}'": {
+#         "is_write_index": true
+#       }
+#     }
+#   }'
+#   curl -XPUT 'http://'${INVENIO_ELASTICSEARCH_HOST}':9200/'${SEARCH_INDEX_PREFIX}'-stats-'${event_name}'-000001' -H 'Content-Type: application/json' -d '
+#   {
+#     "aliases": {
+#       "'${SEARCH_INDEX_PREFIX}'-stats-'${event_name}'": {
+#         "is_write_index": true
+#       }
+#     }
+#   }'
+# done
 # elasticsearch-ilm-setting-end
 
 # sphinxdoc-populate-with-demo-records-begin
